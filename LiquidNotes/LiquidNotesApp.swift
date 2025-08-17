5d7f57943523560c2241caef9aacf57afaacba8a
Chris <anorga2990@gmail.com>
2025-08-17 14:57:11 -0700

    Initial Commit

---
+++ LiquidNotes/LiquidNotesApp.swift
@@ -0,0 +1,21 @@
+//
+//  LiquidNotesApp.swift
+//  LiquidNotes
+//
+//  Created by Christian Anorga on 8/17/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct LiquidNotesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
