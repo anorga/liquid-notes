c7106ef02fa451e0778d2d36fd53a539c40f2a54
Chris <anorga2990@gmail.com>
2025-08-21 20:36:50 -0700

    Update app icon and background design

--- LiquidNotes/Views/Components/LiquidNotesBackground.swift
+++ LiquidNotes/Views/Components/LiquidNotesBackground.swift
@@ -7,18 +7,13 @@
 
 import SwiftUI
 
+// MARK: - iOS 26 Liquid Glass Optimized Background Component
+
 struct LiquidNotesBackground: View {
     var body: some View {
-        LinearGradient(
-            stops: [
-                .init(color: Color(red: 0.0, green: 0.4, blue: 0.8).opacity(0.8), location: 0.0),
-                .init(color: Color.orange.opacity(0.6), location: 0.5),
-                .init(color: Color(red: 0.0, green: 0.35, blue: 0.7).opacity(0.85), location: 1.0)
-            ],
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing
-        )
-        .ignoresSafeArea()
+        // iOS 26: Perfect adaptive background for Liquid Glass
+        Color(.systemGray6)
+            .ignoresSafeArea()
     }
 }
 
